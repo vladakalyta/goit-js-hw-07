fe22f7e38711c59b6765e9b8438b2cef44b21cc0
vladakalyta <vladakalyta@gmail.com>
2022-08-16 18:48:10 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.addEventListener("click", selectGalleryItem);
 
-console.log(galleryItems);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+    </a>
+</div>
+    `;
+    })
+    .join("");
+}
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function selectGalleryItem(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imageOriginal = event.target.dataset.source;
+  const imageAlt = event.target.description;
+
+  const instance = basicLightbox.create(
+    `<img src="${imageOriginal}" alt="${imageAlt}" width="800" height="600">`
+  );
+
+  instance.show();
+}
